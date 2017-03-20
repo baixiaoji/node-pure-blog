6b33c19993a5cd5b823d78fd098a8c8c7600cdac
Kevin <1041874421@qq.com>
2017-03-20 22:09:26 +0800

    重新构造这个服务器代码 1. 高级函数应用 2.新建了一个文件 3. readfile第一个参数的依据来源

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
 // 主要核心入口
 
+const fs = require("fs")
+
 class App {
     constructor() {
 
     }
-    initServer(request, response){
-        response.end("hello world!")
+    initServer() {
+        // 初始化的工作
+        let _package = require("../package")
+        //高级函数返回
+        return (request, response) => {
+            // readFile 异步
+            // readfile的第一个参数 其目录依据是 process.cwd()  node进程的启动目录及 script里的index.js
+            fs.readFile("./public/index.html", "utf-8", (err, data) => {
+                response.end(JSON.stringify(_package))
+            })
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const http = require("http")
 
 const App = require("./app"); // 找 目录下的index.js require(./app/index)
 const Server = new App()
-http.createServer(Server.initServer).listen(7000,  () => {
+http.createServer(Server.initServer()).listen(7000,  () => {
     console.log("监听成功")
 });
