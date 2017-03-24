ebdb75552a91130947122edaf5048e122abacbf4
Kevin <1041874421@qq.com>
2017-03-24 08:44:28 +0800

    根据url不同代码分发

--- app/index.js
+++ app/index.js
@@ -8,14 +8,29 @@ class App {
     }
     initServer() {
         // 初始化的工作
-        let _package = require("../package")
         //高级函数返回
         return (request, response) => {
-            // readFile 异步
-            // readfile的第一个参数 其目录依据是 process.cwd()  node进程的启动目录及 script里的index.js
-            fs.readFile("./public/index.html", "utf-8", (err, data) => {
-                response.end(JSON.stringify(_package))
-            })
+            let { url } = request;
+            // 根据 url 进行代码分发 
+            
+            
+            if(url == "/css/index.css"){
+                fs.readFile("./public/css/index.css","utf-8",(err,data)=>{
+                    response.end(data)
+                })
+            }
+            if(url == "/js/index.js"){
+                fs.readFile("./public/js/index.js","utf-8",(err,data)=>{
+                    response.end(data)
+                })
+            }
+            if(url == "/"){
+                // readfile的第一个参数 其目录依据是 process.cwd()  node进程的启动目录及 script里的index.js
+                fs.readFile("./public/index.html", "utf-8", (err, data) => {
+                    response.end(data)
+                })
+            }
+            
         }
     }
 }
