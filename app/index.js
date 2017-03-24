c42d0fcf2a51e9d65ecc26056155a9de46d325f2
Kevin <1041874421@qq.com>
2017-03-24 08:59:53 +0800

    不要重复代码 1.进行封装

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 // 主要核心入口
 
 const fs = require("fs")
+const path = require("path")
 
 class App {
     constructor() {
@@ -12,25 +13,19 @@ class App {
         return (request, response) => {
             let { url } = request;
             // 根据 url 进行代码分发 
-            
-            
-            if(url == "/css/index.css"){
-                fs.readFile("./public/css/index.css","utf-8",(err,data)=>{
+            let getPath = (url) => {
+                return path.resolve(process.cwd(),"public",`.${url}`)
+            }
+            let staticFunc = (url)=>{
+                if(url == "/"){
+                    url = "/index.html"
+                }
+                let _path = getPath(url);
+                fs.readFile(_path,"utf-8",(err,data)=>{
                     response.end(data)
                 })
             }
-            if(url == "/js/index.js"){
-                fs.readFile("./public/js/index.js","utf-8",(err,data)=>{
-                    response.end(data)
-                })
-            }
-            if(url == "/"){
-                // readfile的第一个参数 其目录依据是 process.cwd()  node进程的启动目录及 script里的index.js
-                fs.readFile("./public/index.html", "utf-8", (err, data) => {
-                    response.end(data)
-                })
-            }
-            
+            staticFunc(url)
         }
     }
 }
