4a1b6d294e7f8a626a3702025c8cbf0738bc9afe
Kevin <1041874421@qq.com>
2017-03-30 21:58:14 +0800

    Promise精华

--- test.js
+++ test.js
@@ -16,21 +16,27 @@
 
 // step 1  new 学习Promise
 let p = new Promise((resolve, reject) => {
-    reject(1)
-}) 
+    setTimeout(reject, 1000, "hello world")
+})
 
 console.log(p)
 
 // then 可以接受两个参数，第一个接受resolve的结果
 // 第二个参数接受reject的结果
+
+// 1. 将这些回调函数  存在处理队列queue
+// 2. 如果promise 已经是fulfilled或rejected的状态，autoRun
 p.then(val => {
     // 处理 resolve 结果
     console.log(`resolve val is ${val}`)
-},val => {
+}, val => {
     // 处理reject的结果
     console.log(`reject val is ${val}`)
 })
 
 // 处理reject层面
-p.catch(val => console.log(`catch val is ${val}`))
+setTimeout(() =>{
+    console.log(p)
+    p.catch(val => console.log(`catch val is ${val}`))
+    }, 3000)
 
