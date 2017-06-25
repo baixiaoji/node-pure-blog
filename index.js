e7ef0e405b71916497f3f0a85406f79e625d2c00
Kevin <1041874421@qq.com>
2017-06-25 15:53:45 +0800

    clear the file restart

--- index.js
+++
@@ -1,9 +0,0 @@
-
-// const 常量
-const http = require("http")
-
-const App = require("./app"); // 找 目录下的index.js require(./app/index)
-const Server = new App()
-http.createServer(Server.initServer()).listen(7000,  () => {
-    console.log("监听成功")
-});
