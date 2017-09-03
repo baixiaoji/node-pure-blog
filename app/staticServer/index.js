0c1f2389b97aa4d45e8fb0947417464ee2bb2665
Kevin <1041874421@qq.com>
2017-09-03 16:29:46 +0800

    [TODO]整理部分代码的冗杂性

--- app/staticServer/index.js
+++ app/staticServer/index.js
@@ -4,25 +4,19 @@ const fs = require("fs")
 
 let getPath = (url) => path.resolve(process.cwd(),"public",`.${url}`)
 let staticFunc = (url) =>{
- 
+    if(url === "/"){
+        url = "/index.html"
+    }
+    let _path = getPath(url)
+
     return new Promise((resolve,reject)=>{
-        if(url === "/"){
-            url = "/index.html"
-        }
-        let _path = getPath(url)
-    
-        let data = ""
-        try{
+     
             fs.readFile(_path,(err,data) =>{
                 resolve(data)
             })
-        }catch(err){
-           reject(`NOT FOUND ${err.stack}`)
-       }
+        
     })
-    
-   
-    //return data
+
 }
 
 module.exports = staticFunc
